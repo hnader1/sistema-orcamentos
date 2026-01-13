24c98aaf14263671544c88040135c3bb51ba66c3
hnader1 <hnader@gmail.com>
2026-01-13 09:54:21 -0300

    Refactor routing in App component with Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Produtos from './pages/Produtos'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
 import Orcamentos from './pages/Orcamentos'
+import OrcamentosStatus from './pages/OrcamentosStatus'
 import OrcamentoForm from './pages/OrcamentoForm'
+import Produtos from './pages/Produtos'
 
-function App() {
+export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/produtos" element={<Produtos />} />
+        {/* Dashboard - Página inicial */}
+        <Route path="/" element={<Dashboard />} />
+        
+        {/* Orçamentos - Lista completa */}
         <Route path="/orcamentos" element={<Orcamentos />} />
+        
+        {/* Orçamentos por Status - Lista filtrada */}
+        <Route path="/orcamentos/status/:status" element={<OrcamentosStatus />} />
+        
+        {/* Criar/Editar Orçamento */}
         <Route path="/orcamentos/novo" element={<OrcamentoForm />} />
         <Route path="/orcamentos/editar/:id" element={<OrcamentoForm />} />
+        
+        {/* Produtos */}
+        <Route path="/produtos" element={<Produtos />} />
+        
+        {/* Redirecionar para dashboard */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
-
-export default App
