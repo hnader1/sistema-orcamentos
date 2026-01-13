4f81b3c22eaac9a80760ccdef41d7a200e79cc75
hnader1 <hnader@gmail.com>
2026-01-13 12:12:00 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
-import Orcamentos from './pages/Orcamentos'
 import OrcamentosStatus from './pages/OrcamentosStatus'
+import Orcamentos from './pages/Orcamentos'
 import OrcamentoForm from './pages/OrcamentoForm'
-import Produtos from './pages/Produtos'
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Dashboard - Página inicial */}
         <Route path="/" element={<Dashboard />} />
         
-        {/* Orçamentos - Lista completa */}
-        <Route path="/orcamentos" element={<Orcamentos />} />
-        
-        {/* Orçamentos por Status - Lista filtrada */}
+        {/* Lista por status */}
         <Route path="/orcamentos/status/:status" element={<OrcamentosStatus />} />
         
-        {/* Criar/Editar Orçamento */}
+        {/* Lista completa */}
+        <Route path="/orcamentos" element={<Orcamentos />} />
+        
+        {/* Criar/Editar */}
         <Route path="/orcamentos/novo" element={<OrcamentoForm />} />
         <Route path="/orcamentos/editar/:id" element={<OrcamentoForm />} />
         
-        {/* Produtos */}
-        <Route path="/produtos" element={<Produtos />} />
-        
-        {/* Redirecionar para dashboard */}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   )
 }
+
