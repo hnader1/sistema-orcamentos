a584fe54e0a929571ae1bffdc9925c065476aed0
hnader1 <hnader@gmail.com>
2026-01-19 16:04:25 -0300

    12312

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,14 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-const options = {
-  auth: {
-    persistSession: true,
-    storageKey: 'construcom-auth',
-    autoRefreshToken: true,
-    detectSessionInUrl: true,
-    flowType: 'pkce'
-  }
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, options)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
