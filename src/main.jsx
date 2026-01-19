1c328172eece7c00776690a5c8e5d23ac3ae18e9
hnader1 <hnader@gmail.com>
2026-01-19 17:01:55 -0300

    12312312

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const hash = window.location.hash
-if (hash && hash.includes('access_token')) {
-  sessionStorage.setItem('supabase-auth-hash', hash)
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
